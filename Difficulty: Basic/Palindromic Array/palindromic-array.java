477de078492422a07833026409f31af4cb5490eb
Nandita Dutta <147627008+nandita994@users.noreply.github.com>
2024-11-07 21:04:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindromic Array/palindromic-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Long> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Long.parseLong(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (long i : array) arr[idx++] = (int)i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.palinArray(arr) ? "true" : "false");
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class Solution {
+    public static boolean palinArray(int[] arr) {
+        // add code here.
+        for(int i=0;i<arr.length;i++){
+            if(!ispalindrome(arr[i])){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean ispalindrome(int n){
+        int on=n;
+        int s=0;
+        while(n!=0){
+            int rem=n%10;
+            s=s*10+rem;
+            n=n/10;
+        }
+        return (on==s);
+    }
+}
